e0ad9be18cb861dd45842c63cc6aedfc965dca55
Adrian Berriel <berriel@gmail.com>
2026-09-08 22:14:40 -0300

    Revert "feat(users): agregado el modulo Users usando el node cli generator"
    This reverts commit 9a50259b7a66640cbc298e01917142a199e86a4e.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { UsersModule } from './users/users.module.js';
 
 @Module({
-  imports: [UsersModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
